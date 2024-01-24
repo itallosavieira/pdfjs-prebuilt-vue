8f3d4e56cb280129a22078347854b8f6c3224e9f
itallo.vieira.glo <itallo.sa.vieira@gmail.com>
2024-01-24 10:05:25 -0300

    feat: add meta url

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 exports.PdfjsIframe = (pdfBlob) => {
+  const currentScriptUrl = new URL(import.meta.url);
+  const basePath = new URL('./pdfjs/web/viewer.html', currentScriptUrl).hre
   return `
     <iframe 
-      src="./pdfjs/web/viewer.html?file=${pdfBlob}" 
+      src="${basePath}?file=${pdfBlob}" 
       width="100%"
       height="1000px">
     </iframe>`;
